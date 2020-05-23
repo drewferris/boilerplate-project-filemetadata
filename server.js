8fb71cd9a886a84b62e04cd6609d1dbb7f24adb6
Drew Ferris <v-drferr@microsoft.com>
2020-05-23 15:38:19 -0700

    challenge completed

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
 
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/hello", function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), function (
+  req,
+  res,
+  next
+) {
+  const { originalname, size } = req.file;
+  res.send({ name: originalname, size });
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+  console.log("Node.js listening ...");
 });
